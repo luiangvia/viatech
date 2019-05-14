a37e9e3b04ea28014168804f3325291bfd4f87e8
luiangvia <luiangvia@gmail.com>
2019-05-13 18:06:45 -0700

    front ends updates

--- client/src/pages/MainPage/MainPageFE.js
+++ client/src/pages/MainPage/MainPageFE.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from '../../headercontainer/headercomponent/Header';
-import Footer from '../../footercontainer/footercomponent/Footer';
+import Header from '../../headercomponent/Header';
+import Footer from '../../footercomponent/Footer';
 import Projectsearchbarcomponent from '../../projectcontainer/projectsearchbarcomponent/Projectsearchbar';
 import Projectlist from '../../projectcontainer/projectlistcomponent/Projectlist';
 import Runlist from '../../projectcontainer/runcontainer/runlist/Runlist';
@@ -14,17 +14,18 @@ import SummaryButton from '../../projectcontainer/runcontainer/summaryReagentsCo
 //example imports below
 import ListProjects from '../../dbProjectDisplayExample/ListProjects';
 import AddProjectForm from '../../dbProjectDisplayExample/AddProjectForm';
+import Logout from '../../logoutcomponent/Logout';
 
 
 
 class MainPageFE extends Component {
 
     state = {
-        showMe: false,
-        showMeCategory: false,
-        showMeRun: false,
-        showMeAddRunForm: false,
-        showMeSummaryReagents: false,
+        // showMe: false,
+        // showMeCategory: false,
+        // showMeRun: false,
+        // showMeAddRunForm: false,
+        // showMeSummaryReagents: false,
     }
 
     operation = (project) => {
@@ -53,11 +54,10 @@ class MainPageFE extends Component {
     render() {
         return (
             <div className='App'>
+                <Logout/>
                 <Header />
-
                 <ListProjects/>
                 <AddProjectForm/>
-
                 <Projectsearchbarcomponent operation={this.operation} />
                 {this.state.showMe ? <Projectlist handleCategory={this.handleCategory} /> : null}
                 {this.state.showMeCategory ? <Projectcategory handleRun={this.handleRun} /> : null}
